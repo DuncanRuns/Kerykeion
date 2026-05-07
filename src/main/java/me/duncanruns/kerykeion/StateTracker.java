e596e4f51f90da6aab19e119140aa5ae14b0af36
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2026-05-07 20:18:58 -0300

    State files are not logs

--- src/main/java/me/duncanruns/kerykeion/StateTracker.java
+++ src/main/java/me/duncanruns/kerykeion/StateTracker.java
@@ -7,19 +7,19 @@ import java.io.IOException;
 import java.util.*;
 
 class StateTracker {
-    private static final Map<HermesInstance, UpdatingJsonFileReader> stateLogs = new HashMap<>();
+    private static final Map<HermesInstance, UpdatingJsonFileReader> stateFiles = new HashMap<>();
 
     public TickResult tick(Collection<HermesInstance> instances) {
         TickResult tickResult = new TickResult();
-        stateLogs.keySet().removeIf(i -> !instances.contains(i));
+        stateFiles.keySet().removeIf(i -> !instances.contains(i));
         for (HermesInstance instance : instances) {
-            UpdatingJsonFileReader reader = stateLogs.computeIfAbsent(instance, i -> new UpdatingJsonFileReader(i.getGameDir().resolve("hermes").resolve("state.json")));
+            UpdatingJsonFileReader reader = stateFiles.computeIfAbsent(instance, i -> new UpdatingJsonFileReader(i.getGameDir().resolve("hermes").resolve("state.json")));
             try {
                 reader.read().ifPresent(
                         json -> tickResult.entries.add(new TickResult.EntryWithInstance(json, instance))
                 );
             } catch (IOException | JsonSyntaxException e) {
-                Kerykeion.errorLogger.accept("Failed to read state log", e);
+                Kerykeion.errorLogger.accept("Failed to read state file", e);
             }
         }
         return tickResult;
